c9b3a76c291bfed33e37370b9851d2d0820cf9f6
Ira Evtikhova <rosieee1408@gmail.com>
2017-12-21 12:47:13 +0300

    add -

--- Functions.cpp
+++ Functions.cpp
@@ -8,9 +8,9 @@ double MySum(double a, double b)
 	//функция суммы
 }
 
-double MySub(double a, double b) 
+double MySub(double a, double b)
 {
-	//функция вычитания
+	return a - b;
 }
 
 double MyMul(double a, double b)
