e0cba81481871a2b24f36fd1cd47770939cd7944
Olga Yanishena <olga.yanishena@mail.ru>
2017-12-21 12:07:30 +0300

    Skelet

---
+++ Functions.cpp
@@ -0,0 +1,24 @@
+// function.cpp
+#include "stdafx.h"
+#include "Functions.h"
+#include <cmath>
+
+double MySum(double a, double b)
+{
+	//функция суммы
+}
+
+double MySub(double a, double b) 
+{
+	//функция вычитания
+}
+
+double MyMul(double a, double b)
+{
+	//функция умножения
+}
+
+double MyDiv(double a, double b)
+{
+	//функция деления
+}
